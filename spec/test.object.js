6ad5718bfd8120e38834f9ebffc20a82cb42fbc0
Katherine McClintic <kdmcclin@gmail.com>
2015-03-03 13:19:40 -0500

    convert spec to jasmine syntax

--- spec/test.object.js
+++ spec/test.object.js
@@ -8,32 +8,32 @@ describe("Object", function(){
       // on separate lines to make them easier to read
 
       expect(frequency(["a", "b", "c", "c", "b", "c", "d"]))
-        .to.deep.equal({ a: 1, b: 2, c: 3, d: 1})
+        .toEqual({ a: 1, b: 2, c: 3, d: 1})
 
       expect(frequency(["a", "b", "c", "d", "e", "f"]))
-        .to.deep.equal({ a: 1, b: 1, c: 1, d: 1, e: 1, f: 1 })
+        .toEqual({ a: 1, b: 1, c: 1, d: 1, e: 1, f: 1 })
 
       expect(frequency(["a", "a", "a", "a", "a"]))
-        .to.deep.equal({ a: 5 });
+        .toEqual({ a: 5 });
 
       expect(frequency([]))
-        .to.deep.equal({});
+        .toEqual({});
 
     });
 
     it("should have a unique items function", function(){
 
       expect(unique(["d", "b", "c", "c", "b", "c", "a"]))
-        .to.include.members(["a", "b", "c", "d"]);
+        .toContain(["a", "b", "c", "d"]);
 
       expect(unique(["a", "b", "c", "d", "e", "f"]))
-        .to.include.members(["a", "b", "c", "d", "e", "f"]);
+        .toContain(["a", "b", "c", "d", "e", "f"]);
 
       expect(unique(["a", "a", "a", "a", "a"]))
-        .to.deep.equal(["a"]);
+        .toEqual(["a"]);
 
       expect(unique([]))
-        .to.deep.equal([]);
+        .toEqual([]);
 
     });
 
@@ -43,22 +43,22 @@ describe("Object", function(){
 
     it("should have a tripler function", function(){
 
-      expect(tripler(3)).to.equal(9);
-      expect(tripler(10)).to.equal(30);
-      expect(tripler(-5)).to.equal(-15);
+      expect(tripler(3)).toEqual(9);
+      expect(tripler(10)).toEqual(30);
+      expect(tripler(-5)).toEqual(-15);
 
     });
 
     it("should have an object tripler function", function(){
 
       expect(objectTripler({ a: 1, b: 2, c: 3 }))
-        .to.deep.equal({ a: 3, b: 6, c: 9 });
+        .toEqual({ a: 3, b: 6, c: 9 });
 
       expect(objectTripler({ x: -10, y: 30, z: 0 }))
-        .to.deep.equal({ x: -30, y: 90, z: 0 });
+        .toEqual({ x: -30, y: 90, z: 0 });
 
       expect(objectTripler({}))
-        .to.deep.equal({});
+        .toEqual({});
 
     });
 
@@ -69,23 +69,23 @@ describe("Object", function(){
     it("should be okay with objects without conflicts", function(){
 
       expect(extend({ a: 1, b: 2 }, { c: 3, d: 4 }))
-        .to.deep.equal({ a: 1, b: 2, c: 3, d: 4 });
+        .toEqual({ a: 1, b: 2, c: 3, d: 4 });
 
       expect(extend({ a: 1, b: 2 }, {}))
-        .to.deep.equal({ a: 1, b: 2 });
+        .toEqual({ a: 1, b: 2 });
 
       expect(extend({}, { c: 3 }))
-        .to.deep.equal({ c: 3 });
+        .toEqual({ c: 3 });
 
     });
 
     it("should be okay with objects WITH conflicts", function(){
 
       expect(extend({ a: 1, b: 2 }, { b: 3, c: 4 }))
-        .to.deep.equal({ a: 1, b: 2, c: 4 });
+        .toEqual({ a: 1, b: 2, c: 4 });
 
       expect(extend({ a: 1, b: 2, c: 3 }, { b: 9 }))
-        .to.deep.equal({ a: 1, b: 2, c: 3 });
+        .toEqual({ a: 1, b: 2, c: 3 });
 
     });
 
@@ -96,7 +96,7 @@ describe("Object", function(){
     it("should work correctly", function(){
 
       expect(pluck([ { a: 1, b: 2 }, { a: 3 }, { a: 4, c: 5 }], "a"))
-        .to.deep.equal([1, 3, 5]);
+        .toEqual([1, 3, 5]);
 
     });
 
